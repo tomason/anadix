aad5be8c86398e9160e734448f158cd620c5d6f1
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-08-16 23:25:03 +0200

    api: changed ObjectFactory methods - all of them use InstantiationException with the same message format, the default class names are modifiable through constants and method to create default analyzer was added

--- analyzer-api/src/main/java/org/analyzer/factories/ObjectFactory.java
+++ analyzer-api/src/main/java/org/analyzer/factories/ObjectFactory.java
@@ -9,36 +9,72 @@ import org.analyzer.Parser;
 import org.analyzer.exceptions.InstantiationException;
 
 public abstract class ObjectFactory {
-	public static Parser newParser(String className) throws Exception {
-		return (Parser)Class.forName(className).newInstance();
+	private static final String defaultParser = "org.analyzer.swingparser.SwingParser";
+	private static final String defaultConditions = "org.analyzer.section508.Section508";
+	private static final String defaultAnalyzer = "org.analyzer.impl.AnalyzerImpl";
+	private static final String defaultElementFactory = "org.analyzer.html.HTMLElementFactory";
+
+	private static final String errorMessaageFormat = "Could not instantiate %s: %s";
+
+	public static Parser newParser(String className) throws InstantiationException {
+		if (className == null || className.length() == 0) {
+			className = defaultParser;
+		}
+
+		try {
+			return (Parser)Class.forName(className).newInstance();
+		} catch (Exception ex) {
+			throw newInstantiationException("Parser", className, ex);
+		}
 	}
 
-	public static ConditionSet newConditionSet(String className) throws Exception {
-		return (ConditionSet)Class.forName(className).newInstance();
+	public static ConditionSet newConditionSet(String className) throws InstantiationException {
+		if (className == null) {
+			className = defaultConditions;
+		}
+		try {
+			return (ConditionSet)Class.forName(className).newInstance();
+		} catch (Exception ex) {
+			throw newInstantiationException("ConditionSet", className, ex);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
 	public static <T extends ElementFactory>
 	Class<T> getElementFactoryClass(String className) throws InstantiationException {
 		if (className == null || className.length() == 0) {
-			className = "org.analyzer.html.HTMLElementFactory";
+			className = defaultElementFactory;
 		}
 		try {
 			return (Class<T>)Class.forName(className);
-		} catch (ClassNotFoundException ex) {
-			throw new InstantiationException("Could not instantiate ElementFactory", ex);
+		} catch (Exception ex) {
+			throw newInstantiationException("ElementFactory", className, ex);
 		}
+	}
+
+	public static Analyzer newAnalyzer() throws InstantiationException {
+		return newAnalyzer(null, null);
 	}
 
 	@SuppressWarnings("unchecked")
 	public static Analyzer newAnalyzer(String parserClassName, String conditionClassName)
-			throws Exception {
-		Class<Analyzer> clazz = (Class<Analyzer>)Class.forName("org.analyzer.impl.AnalyzerImpl");
-		Constructor<Analyzer> c = clazz.getConstructor(
-				Parser.class, ConditionSet.class);
+			throws InstantiationException {
+		try {
+			Class<Analyzer> clazz = (Class<Analyzer>)Class.forName(defaultAnalyzer);
+			Constructor<Analyzer> c = clazz.getConstructor(
+					Parser.class, ConditionSet.class);
 
-		return c.newInstance(
-				newParser(parserClassName),
-				newConditionSet(conditionClassName));
+			return c.newInstance(
+					newParser(parserClassName),
+					newConditionSet(conditionClassName));
+		} catch (Exception ex) {
+			throw newInstantiationException("Analyzer", defaultAnalyzer, ex);
+		}
+	}
+
+	private static InstantiationException newInstantiationException(String type, String className,
+			Throwable cause) {
+		return new InstantiationException(
+				String.format(errorMessaageFormat, type, className), cause);
 	}
 }
