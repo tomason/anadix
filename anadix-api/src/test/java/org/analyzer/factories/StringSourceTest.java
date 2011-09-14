100098f59e4a73eae3b5a7531790d6c4b81c2e37
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-09-14 23:48:32 +0200

    api: another unit test (#11)

---
+++ anadix-api/src/test/java/org/analyzer/factories/StringSourceTest.java
@@ -0,0 +1,79 @@
+package org.analyzer.factories;
+
+import static org.testng.Assert.*;
+
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.analyzer.Source;
+import org.testng.annotations.Test;
+
+
+public class StringSourceTest extends SourceTestTemplate {
+	private final String description = "test description";
+
+	@Test(groups = "constructor", expectedExceptions = NullPointerException.class)
+	public void testNewStringSource1() throws Exception {
+		SourceFactory.newStringSource(null);
+	}
+
+	@Test(groups = "constructor", expectedExceptions = NullPointerException.class)
+	public void testNewStringSource2() throws Exception {
+		SourceFactory.newStringSource(null, null);
+	}
+
+	@Test(groups = "constructor", expectedExceptions = NullPointerException.class)
+	public void testNewStringSource3() throws Exception {
+		SourceFactory.newStringSource(null, description);
+	}
+
+	@Test(groups = "constructor")
+	public void testNewStringSource4() throws Exception {
+		SourceFactory.newStringSource(sourceText, null);
+	}
+
+	@Test(groups = "constructor")
+	public void testNewStringSource5() throws Exception {
+		SourceFactory.newStringSource(sourceText, description);
+	}
+
+	@Test(dependsOnGroups = "constructor")
+	public void testGetDescription() throws Exception {
+		Source s = SourceFactory.newStringSource(sourceText, description);
+
+		assertEquals(s.getDescription(), description);
+	}
+
+	@Test(dependsOnGroups = "constructor")
+	public void testGetStream() throws Exception {
+		Source s = SourceFactory.newStringSource(sourceText);
+
+		InputStream is = s.getStream();
+
+		assertNotNull(is);
+		String content = readStream(is);
+		assertNotNull(content);
+		assertEquals(content, sourceText);
+	}
+
+	@Test(dependsOnGroups = "constructor")
+	public void testGetReader() throws Exception {
+		Source s = SourceFactory.newStringSource(sourceText);
+
+		Reader r = s.getReader();
+
+		assertNotNull(r);
+		String content = readReader(r);
+		assertNotNull(content);
+		assertEquals(content, sourceText);
+	}
+
+	@Test(dependsOnGroups = "constructor")
+	public void testGetText() throws Exception {
+		Source s = SourceFactory.newStringSource(sourceText);
+		String content = s.getText();
+
+		assertNotNull(content);
+		assertEquals(content, sourceText);
+	}
+}
