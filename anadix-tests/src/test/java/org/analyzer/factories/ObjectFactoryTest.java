ec2131bedb523decfdac71c5a3760b82ffead567
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-09-17 22:55:02 +0200

    tests: added tests to ObjectFactoryTest (#11)

--- anadix-tests/src/test/java/org/analyzer/factories/ObjectFactoryTest.java
+++ anadix-tests/src/test/java/org/analyzer/factories/ObjectFactoryTest.java
@@ -5,6 +5,10 @@ import static org.testng.Assert.*;
 
 import org.analyzer.ConditionSet;
 import org.analyzer.Parser;
+import org.analyzer.ReportFormatter;
+import org.analyzer.mock.MockConditionSet;
+import org.analyzer.mock.MockParser;
+import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
 @Test
@@ -24,8 +28,13 @@ public class ObjectFactoryTest {
 		ObjectFactory.newParser("non.existent.Class");
 	}
 
-	@Test(groups = "parser")
+	@Test(groups = "parser", expectedExceptions = InstantiationException.class)
 	public void testNewParser4() throws Exception {
+		ObjectFactory.newParser("java.lang.Object");
+	}
+
+	@Test(groups = "parser")
+	public void testNewParser5() throws Exception {
 		Parser expected = mock(Parser.class);
 		Parser p = ObjectFactory.newParser(expected.getClass().getCanonicalName());
 
@@ -34,12 +43,12 @@ public class ObjectFactoryTest {
 	}
 
 	@Test(groups = "parser", expectedExceptions = NullPointerException.class)
-	public void testNewParser5() throws Exception {
+	public void testNewParser6() throws Exception {
 		ObjectFactory.newParser((Class<? extends Parser>)null);
 	}
 
 	@Test(groups = "parser")
-	public void testNewParser6() throws Exception {
+	public void testNewParser7() throws Exception {
 		Parser expected = mock(Parser.class);
 		Parser p = ObjectFactory.newParser(expected.getClass());
 
@@ -62,8 +71,13 @@ public class ObjectFactoryTest {
 		ObjectFactory.newConditionSet("non.existent.Class");
 	}
 
-	@Test(groups = "conditions")
+	@Test(groups = "conditions", expectedExceptions = InstantiationException.class)
 	public void testNewConditionSet4() throws Exception {
+		ObjectFactory.newConditionSet("java.lang.Object");
+	}
+
+	@Test(groups = "conditions")
+	public void testNewConditionSet5() throws Exception {
 		ConditionSet expected = mock(ConditionSet.class);
 		ConditionSet cs = ObjectFactory.newConditionSet(expected.getClass().getCanonicalName());
 
@@ -72,12 +86,12 @@ public class ObjectFactoryTest {
 	}
 
 	@Test(groups = "conditions", expectedExceptions = NullPointerException.class)
-	public void testNewConditionSet5() throws Exception {
+	public void testNewConditionSet6() throws Exception {
 		ObjectFactory.newConditionSet((Class<? extends ConditionSet>)null);
 	}
 
 	@Test(groups = "conditions")
-	public void testNewConditionSet6() throws Exception {
+	public void testNewConditionSet7() throws Exception {
 		ConditionSet expected = mock(ConditionSet.class);
 		ConditionSet cs = ObjectFactory.newConditionSet(expected.getClass());
 
@@ -85,33 +99,107 @@ public class ObjectFactoryTest {
 		assertEquals(cs.getClass(), expected.getClass());
 	}
 
-	/* FIXME the rest of the tests should go to integration tests
-	 * as this module doesn't contain the necessary dependencies
 	@Test(dependsOnGroups = { "parser", "conditions" })
-	public void testNewAnalyzer1() {
-		try {
-			ObjectFactory.newAnalyzer();
-		} catch (InstantiationException ex) {
-			// this will happen as we don't have the correct implementation
-		}
+	public void testNewAnalyzer1() throws Exception {
+		ObjectFactory.newAnalyzer();
 	}
+
 
 	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
 	public void testNewAnalyzer2() throws Exception {
 		ObjectFactory.newAnalyzer(null);
 	}
 
-	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	@Test(dependsOnGroups = { "parser", "conditions" })
 	public void testNewAnalyzer3() throws Exception {
-		ConditionSet cs = mock(ConditionSet.class);
-		when(cs.getDefaultParser()).thenAnswer(null);
+		ObjectFactory.newAnalyzer(MockConditionSet.class);
+	}
 
-		ObjectFactory.newAnalyzer(cs.getClass());
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer4() throws Exception {
+		ObjectFactory.newAnalyzer((Class<Parser>)null, null);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer5() throws Exception {
+		ObjectFactory.newAnalyzer(null, MockConditionSet.class);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer6() throws Exception {
+		ObjectFactory.newAnalyzer(MockParser.class, null);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer7() throws Exception {
+		ObjectFactory.newAnalyzer(MockParser.class, null);
 	}
 
 	@Test(dependsOnGroups = { "parser", "conditions" })
-	public void testNewAnalyzer4() throws Exception {
+	public void testNewAnalyzer8() throws Exception {
+		ObjectFactory.newAnalyzer(MockParser.class, MockConditionSet.class);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer9() throws Exception {
 		ObjectFactory.newAnalyzer((Parser)null, null);
 	}
-	 */
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer10() throws Exception {
+		ObjectFactory.newAnalyzer(new MockParser(), null);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewAnalyzer11() throws Exception {
+		ObjectFactory.newAnalyzer(null, new MockConditionSet());
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" })
+	public void testNewAnalyzer12() throws Exception {
+		ObjectFactory.newAnalyzer(new MockParser(), new MockConditionSet());
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" })
+	public void testNewFormatter1() throws Exception {
+		ObjectFactory.newFormatter();
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewFormatter2() throws Exception {
+		ObjectFactory.newFormatter((String)null);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewFormatter3() throws Exception {
+		ObjectFactory.newFormatter("");
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = InstantiationException.class)
+	public void testNewFormatter4() throws Exception {
+		ObjectFactory.newFormatter("non.existing.ClassReportFormatter");
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = InstantiationException.class)
+	public void testNewFormatter5() throws Exception {
+		ObjectFactory.newFormatter("java.lang.Object");
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" })
+	public void testNewFormatter6() throws Exception {
+		ReportFormatter rf = Mockito.mock(ReportFormatter.class);
+		ObjectFactory.newFormatter(rf.getClass().getName());
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" }, expectedExceptions = NullPointerException.class)
+	public void testNewFormatter7() throws Exception {
+		ObjectFactory.newFormatter((Class<ReportFormatter>)null);
+	}
+
+	@Test(dependsOnGroups = { "parser", "conditions" })
+	public void testNewFormatter8() throws Exception {
+		ReportFormatter rf = Mockito.mock(ReportFormatter.class);
+
+		ObjectFactory.newFormatter(rf.getClass());
+	}
 }
