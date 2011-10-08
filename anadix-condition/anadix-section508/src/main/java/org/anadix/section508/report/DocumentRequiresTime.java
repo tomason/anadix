1bae5c16b22711a3983e2edbbb5e1ad18c2c844d
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-10-08 15:52:46 +0200

    section508: paragraph p) implementation (closes #34)

---
+++ anadix-condition/anadix-section508/src/main/java/org/anadix/section508/report/DocumentRequiresTime.java
@@ -0,0 +1,12 @@
+package org.anadix.section508.report;
+
+import org.anadix.ItemStatus;
+
+public class DocumentRequiresTime extends Section508ReportItem {
+	private static final String KEY = "document.requires.time";
+
+	public DocumentRequiresTime() {
+		super(ItemStatus.MANUAL, KEY);
+	}
+
+}
