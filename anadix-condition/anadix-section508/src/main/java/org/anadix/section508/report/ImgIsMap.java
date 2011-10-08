fdab5a1e9e7bc9b884eccd85b5447253f09e489d
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-10-08 13:24:44 +0200

    section508: paragraph f) implementation (closes #24)

--- anadix-condition/anadix-section508/src/main/java/org/anadix/section508/report/ImgIsMap.java
+++ anadix-condition/anadix-section508/src/main/java/org/anadix/section508/report/ImgIsMap.java
@@ -4,13 +4,19 @@ import org.anadix.ItemStatus;
 import org.anadix.html.ImgTag;
 
 public class ImgIsMap extends Section508ReportItem {
-	private static final String KEY = "img.is.map";
+	private static final String KEY_MANUAL = "img.has.textlinks";
+	private static final String KEY_WARNING = "img.is.map";
 
 	public ImgIsMap(ImgTag cause) {
-		super(ItemStatus.MANUAL, KEY, cause.getSource(), cause.getPosition());
-
-		setDescription(formatDescriptionString(KEY));
-		setAdvice(formatAdviceString(KEY));
+		this(cause, false);
 	}
 
+	public ImgIsMap(ImgTag cause, boolean warning) {
+		super((warning ? ItemStatus.WARNING : ItemStatus.MANUAL),
+				(warning ? KEY_WARNING : KEY_MANUAL),
+				cause.getSource(), cause.getPosition());
+
+		setDescription(formatDescriptionString(warning ? KEY_WARNING : KEY_MANUAL));
+		setAdvice(formatAdviceString(warning ? KEY_WARNING : KEY_MANUAL));
+	}
 }
