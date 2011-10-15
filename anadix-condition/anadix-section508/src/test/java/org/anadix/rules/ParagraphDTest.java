806068cdb758e5bf047b08aa61f6137e41f3b83d
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-10-16 00:30:09 +0200

    section508: few new tests (#17)

---
+++ anadix-condition/anadix-section508/src/test/java/org/anadix/rules/ParagraphDTest.java
@@ -0,0 +1,76 @@
+package org.anadix.rules;
+
+import java.util.Collection;
+import java.util.Properties;
+
+import org.anadix.ReportItem;
+import org.anadix.html.HtmlElement;
+import org.anadix.html.LinkTag;
+import org.anadix.section508.report.DocumentContainsStyle;
+import org.anadix.section508.report.ElementContainsStyle;
+
+public class ParagraphDTest extends RulesetTest {
+	public ParagraphDTest() {
+		super("paragraph-d.drl");
+	}
+
+	/*
+	 * rule "stylesheets present"
+	 */
+	public void testRule1_1() {
+		Properties attributes = new Properties();
+		attributes.setProperty("rel", "stylesheet");
+		LinkTag tag = factory.createLinkTag(dummyId, html, attributes);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportContains(items, DocumentContainsStyle.class);
+	}
+
+	public void testRule1_2() {
+		LinkTag tag = factory.createLinkTag(dummyId, html, dummyPropeties);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportNotContains(items, DocumentContainsStyle.class);
+	}
+
+	public void testRule1_3() {
+		Properties attributes = new Properties();
+		attributes.setProperty("style", "color: #FFFFFF");
+		HtmlElement tag = factory.createH1Tag(dummyId, body, attributes);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportContains(items, DocumentContainsStyle.class);
+	}
+
+	public void testRule1_4() {
+		HtmlElement tag = factory.createH1Tag(dummyId, body, dummyPropeties);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportNotContains(items, DocumentContainsStyle.class);
+	}
+
+	/*
+	 * rule "styles in tags"
+	 */
+	public void testRule2_1() {
+		Properties attributes = new Properties();
+		attributes.setProperty("style", "color: #FFFFFF");
+		HtmlElement tag = factory.createH1Tag(dummyId, body, attributes);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportContains(items, ElementContainsStyle.class, tag.getSource());
+	}
+
+	public void testRule2_2() {
+		HtmlElement tag = factory.createH1Tag(dummyId, body, dummyPropeties);
+		tag.setSource(dummySource);
+		Collection<ReportItem> items = evaluate(tag);
+
+		assertReportNotContains(items, ElementContainsStyle.class, tag.getSource());
+	}
+}
