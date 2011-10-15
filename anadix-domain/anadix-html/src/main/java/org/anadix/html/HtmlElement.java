83858ff4b44487681e65c81364068975bddbce4b
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-10-15 13:35:18 +0200

    html: implementation of hashCode, equals and compareTo methods

--- anadix-domain/anadix-html/src/main/java/org/anadix/html/HtmlElement.java
+++ anadix-domain/anadix-html/src/main/java/org/anadix/html/HtmlElement.java
@@ -19,7 +19,7 @@ import java.math.BigInteger;
 
 import org.anadix.Element;
 
-public abstract class HtmlElement implements Element {
+public abstract class HtmlElement implements Element, Comparable<HtmlElement> {
 	private final BigInteger id;
 	private final String name;
 	private final HtmlElement parent;
@@ -117,6 +117,40 @@ public abstract class HtmlElement implements Element {
 		}
 
 		this.textContent = textContent;
+	}
+
+	public int compareTo(HtmlElement o) {
+		return getId().compareTo(o.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		HtmlElement other = (HtmlElement) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
