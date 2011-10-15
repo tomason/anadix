ca51b04ec727b4fc0923eac3ed10abe37f035c36
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-10-15 21:45:57 +0200

    section508: paragraph j) implementation (#28)

--- anadix-condition/anadix-section508/src/main/java/org/anadix/section508/report/ElementCauseFlickering.java
+++ anadix-condition/anadix-section508/src/main/java/org/anadix/section508/report/ElementCauseFlickering.java
@@ -9,6 +9,10 @@ import org.anadix.html.ObjectTag;
 public class ElementCauseFlickering extends Section508ReportItem {
 	private static final String KEY_SUFFIX = ".cause.flickering";
 
+	public ElementCauseFlickering() {
+		this(ItemStatus.MANUAL, "document" + KEY_SUFFIX, null);
+	}
+
 	public ElementCauseFlickering(ObjectTag cause) {
 		this(ItemStatus.MANUAL, "object" + KEY_SUFFIX, cause);
 	}
@@ -21,7 +25,12 @@ public class ElementCauseFlickering extends Section508ReportItem {
 		this(ItemStatus.MANUAL, "img" + KEY_SUFFIX, cause);
 	}
 
-	private ElementCauseFlickering(ItemStatus status, String key, HtmlElement cause) {
-		super(status, key, cause.getSource(), cause.getPosition());
+	public ElementCauseFlickering(ItemStatus status, String key, HtmlElement cause) {
+		super(status, key,
+				cause == null ? new Object[0] : new Object[] { cause.getSource(), cause.getPosition() }
+				);
+
+		setDescription(formatDescriptionString(key));
+		setAdvice(formatAdviceString(key));
 	}
 }
