50b90e195ec03142f6c0fc3ea39b76033ae60607
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-11-22 17:23:36 +0100

    html? javadoc comments for Attributes class (#37)

--- anadix-domain/anadix-html/src/main/java/org/anadix/html/Attributes.java
+++ anadix-domain/anadix-html/src/main/java/org/anadix/html/Attributes.java
@@ -23,13 +23,18 @@ import java.util.regex.Pattern;
 /**
  * Class representing set of element's attributes. Contains both the attributes
  * contained in tag and contained in style.
- * 
- * @author tomason
  *
+ * @author tomason
+ * @version $Id: $
  */
 public class Attributes {
 	private final Properties attributes;
 
+	/**
+	 * Constructor
+	 *
+	 * @param attributes list of key - value pairs
+	 */
 	public Attributes(Properties attributes) {
 		if (attributes == null) {
 			throw new NullPointerException("attributes can't be null");
@@ -48,22 +53,52 @@ public class Attributes {
 		}
 	}
 
+	/**
+	 * Gets the attribute by name
+	 *
+	 * @param key name of the attribute to get
+	 * @return value of the attribute or null if attribute is not defined
+	 */
 	public String getAttribute(String key) {
 		return attributes.getProperty(key);
 	}
 
+	/**
+	 * Gets the attribute by name
+	 *
+	 * @param key name of the attribute to get
+	 * @param defaultValue default value to use if attribute is not defined
+	 * @return value of the attribute or defaultValue if attribute is not defined
+	 */
 	public String getAttribute(String key, String defaultValue) {
 		return attributes.getProperty(key, defaultValue);
 	}
 
+	/**
+	 * Determines whether attribute with given name is defined
+	 *
+	 * @param key name of the attribute
+	 * @return true if attribute is defined false otherwise
+	 */
 	public boolean hasAttribute(String key) {
 		return attributes.containsKey(key);
 	}
 
+	/**
+	 * Gets names of all attributes 
+	 *
+	 * @return Collection of attribute names
+	 */
 	public Collection<String> getAvaliableAttributes() {
 		return attributes.stringPropertyNames();
 	}
 
+	/**
+	 * Parses the style into list of key - value pairs
+	 *
+	 * @param style String representation of style
+	 * @return list of key - value pairs
+	 */
 	public static Properties parseStyle(String style) {
 		Properties result = new Properties();
 
@@ -84,6 +119,7 @@ public class Attributes {
 		return result;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public String toString() {
 		return attributes.toString();
