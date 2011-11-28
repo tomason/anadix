1f1b1367192d56bd37e002552efd97fb7ebc4a42
Tomas Schlosser <Schlosser.T@seznam.cz>
2011-11-28 23:22:20 +0100

    example: setting XHTMLReportFormatter as default formatter

--- anadix-example/anadix-sample/src/main/java/org/anadix/sample/App.java
+++ anadix-example/anadix-sample/src/main/java/org/anadix/sample/App.java
@@ -8,6 +8,7 @@ import org.anadix.Analyzer;
 import org.anadix.Report;
 import org.anadix.exceptions.SourceException;
 import org.anadix.factories.SourceFactory;
+import org.anadix.impl.XHTMLReportFormatter;
 import org.anadix.section508.Section508;
 
 /**
@@ -24,19 +25,25 @@ public class App {
 	 */
 	public static void main( String[] args ) {
 		String[] filenames = new String[] {
+				//"Drools-5.2.0.html",	//commented out as it takes the longest
 				"google.com.html",
 				"idnes.cz.html",
 				"seznam.cz.html",
-				"redhat.com.html",
-				"Drools-5.2.0.html"
+				"redhat.com.html"
 		};
+		
+		Anadix.setDefaultFormatter(XHTMLReportFormatter.class);
+		
 		try {
 			Analyzer a = Anadix.newAnalyzer(new Section508());
 			for (String filename: filenames) {
 				Report r = a.analyze(SourceFactory.newClassPathSource("/" + filename));
+				Anadix.formatReport(r);
 				System.out.println(r.report());
 			}
+			
 			Report r = a.analyze(SourceFactory.newURLSource(new URL("http://section508.gov")));
+			Anadix.formatReport(r);
 			System.out.println(r.report());
 		} catch (InstantiationException e) {
 			e.printStackTrace();
